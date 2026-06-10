18d80e00c0aab63a51140afec36dae0158906478
Mahima Chaudhary <chaudharymahima983@gmail.com>
2026-06-09 19:19:59 -0500

    env configure

--- utils/db.py
+++ utils/db.py
@@ -1,11 +1,13 @@
+import os
+from dotenv import load_dotenv
 import psycopg2
 
 
 def get_connection():
     return psycopg2.connect(
         host="localhost",
-        port=5434,
-        database="datahireiq",
-        user="datahireiq_user",
-        password="datahireiq_pass"
+        port=os.getenv("POSTGRES_PORT"),
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
     )
